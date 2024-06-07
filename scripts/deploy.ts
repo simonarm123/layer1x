caff60fbcf8aabcfc63d4f6648b36bdd0a6cabd2
simonarm123 <glu1n50@shitmail.org>
2024-06-07 16:42:47 +0100

    init

---
+++ scripts/deploy.ts
@@ -0,0 +1,24 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Token = await ethers.getContractFactory("Token");
+  const initialSupply = "1000000000000000000000"; // 1000 tokens
+
+  const token = await Token.deploy(initialSupply);
+
+  let tx = token.deploymentTransaction();
+
+  console.log("Token deployed to: ", await token.getAddress());
+  console.log("Tx hash: ", tx.hash);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
